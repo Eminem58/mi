c44802d88e0f1a9c56052cca85912a02e223460f
金彪 <459840682@qq.com>
2021-01-18 23:05:07 +0800

    Initial commit

---
+++ mi-parent/src/main/java/com/mi/miparent/MiParentApplication.java
@@ -0,0 +1,13 @@
+package com.mi.miparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiParentApplication.class, args);
+    }
+
+}
---
+++ mi-parent/src/test/java/com/mi/miparent/MiParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mi.miparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
